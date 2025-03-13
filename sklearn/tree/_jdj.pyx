02c15cee032b1631cd79265d199418f740cbb8da
Luis Llana <llana@sip.ucm.es>
2025-03-13 20:57:09 +0100

    clonar jini

---
+++ sklearn/tree/_jdj.pyx
@@ -0,0 +1,85 @@
+from ._criterion cimport ClassificationCriterion, float64_t, intp_t
+
+cdef class JDJ(ClassificationCriterion):
+    r"""JDJ Index polarization criterion.
+
+    This handles cases where the target is a classification taking values
+    0, 1, ... K-2, K-1. If node m represents a region Rm with Nm observations,
+    then let
+
+        count_k = 1/ Nm \sum_{x_i in Rm} I(yi = k)
+
+    be the proportion of class k observations in node m.
+
+    The Gini Index is then defined as:
+
+        index = \sum_{k=0}^{K-1} count_k (1 - count_k)
+              = 1 - \sum_{k=0}^{K-1} count_k ** 2
+    """
+
+    cdef float64_t node_impurity(self) noexcept nogil:
+        """Evaluate the impurity of the current node.
+
+        Evaluate the Gini criterion as impurity of the current node,
+        i.e. the impurity of sample_indices[start:end]. The smaller the impurity the
+        better.
+        """
+        cdef float64_t gini = 0.0
+        cdef float64_t sq_count
+        cdef float64_t count_k
+        cdef intp_t k
+        cdef intp_t c
+
+        for k in range(self.n_outputs):
+            sq_count = 0.0
+
+            for c in range(self.n_classes[k]):
+                count_k = self.sum_total[k, c]
+                sq_count += count_k * count_k
+
+            gini += 1.0 - sq_count / (self.weighted_n_node_samples *
+                                      self.weighted_n_node_samples)
+
+        return gini / self.n_outputs
+
+    cdef void children_impurity(self, float64_t* impurity_left,
+                                float64_t* impurity_right) noexcept nogil:
+        """Evaluate the impurity in children nodes.
+
+        i.e. the impurity of the left child (sample_indices[start:pos]) and the
+        impurity the right child (sample_indices[pos:end]) using the Gini index.
+
+        Parameters
+        ----------
+        impurity_left : float64_t pointer
+            The memory address to save the impurity of the left node to
+        impurity_right : float64_t pointer
+            The memory address to save the impurity of the right node to
+        """
+        cdef float64_t gini_left = 0.0
+        cdef float64_t gini_right = 0.0
+        cdef float64_t sq_count_left
+        cdef float64_t sq_count_right
+        cdef float64_t count_k
+        cdef intp_t k
+        cdef intp_t c
+
+        for k in range(self.n_outputs):
+            sq_count_left = 0.0
+            sq_count_right = 0.0
+
+            for c in range(self.n_classes[k]):
+                count_k = self.sum_left[k, c]
+                sq_count_left += count_k * count_k
+
+                count_k = self.sum_right[k, c]
+                sq_count_right += count_k * count_k
+
+            gini_left += 1.0 - sq_count_left / (self.weighted_n_left *
+                                                self.weighted_n_left)
+
+            gini_right += 1.0 - sq_count_right / (self.weighted_n_right *
+                                                  self.weighted_n_right)
+
+        impurity_left[0] = gini_left / self.n_outputs
+        impurity_right[0] = gini_right / self.n_outputs
